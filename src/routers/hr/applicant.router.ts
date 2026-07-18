6f63c550ea5c2671b4a1baa5d250d84cd482b7b0
GautamKumarhr7 <kumargautam3231@gmail.com>
2026-07-18 20:25:07 +0530

    commited

--- src/routers/hr/applicant.router.ts
+++ src/routers/hr/applicant.router.ts
@@ -7,7 +7,7 @@ import path from 'path';
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'uploads/job/');
+    cb(null, path.join(process.cwd(), 'uploads/job'));
   },
   filename: (req, file, cb) => {
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
